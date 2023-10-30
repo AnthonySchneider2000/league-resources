1bb867b82456836c7561a6c9e2c977a24c9b1a4d
Anthony Schneider <tonyschneider3@gmail.com>
2023-10-30 18:07:36 -0500

    stats: added xp/gold to constants.ts

---
+++ lib/constants.ts
@@ -0,0 +1,134 @@
+export const caster = {
+    "gold": 14,
+    "experience": 30,
+}
+
+export const melee = {
+    "gold": 21,
+    "experience": 61.75,
+}
+
+export const topCannon = {
+    "gold": 69,
+    "experience": 95,
+}
+
+export const midCannon = {
+    "gold": 63,
+    "experience": 95,
+}
+
+export const botCannon = {
+    "gold": 61,
+    "experience": 95,
+}
+
+export const plating = {
+    "gold": 125,
+    "experience": 0,
+}
+
+export const outerTurret = {
+    "gold": 500,
+    "experience": 0,
+}
+
+export const innerTurret = {
+    "gold": 800,
+    "experience": 0,
+}
+
+export const innerTurretMid = {
+    "gold": 550,
+    "experience": 0,
+}
+
+export const inhibTurret = {
+    "gold": 300,
+    "experience": 0,
+}
+
+export const nexusTurret = {
+    "gold": 250,
+    "experience": 0,
+}
+
+export const redBuff = {
+    "gold": 90,
+    "experience": 95,
+}
+
+export const blueBuff = {
+    "gold": 90,
+    "experience": 95,
+}
+
+export const bigRaptor = {
+    "gold": 35,
+    "experience": 20,
+}
+
+export const smallRaptor = {
+    "gold": 8,
+    "experience": 10,
+}
+
+export const bigWolf = {
+    "gold": 55,
+    "experience": 50,
+}
+
+export const smallWolf = {
+    "gold": 13,
+    "experience": 15,
+}
+
+export const bigKrug = {
+    "gold": 15,
+    "experience": 15,
+}
+
+export const mediumKrug = {
+    "gold": 10,
+    "experience": 10,
+}
+
+export const smallKrug = {
+    "gold": 14,
+    "experience": 10,
+}
+
+export const gromp = {
+    "gold": 80,
+    "experience": 120,
+}
+
+export const scuttle = {
+    "gold": 55,
+    "experience": 100,
+}
+
+export const dragon = {
+    "gold": 25,
+    "experience": 150,
+}
+
+export const herald = {
+    "gold": 200,
+    "experience": 306,
+}
+
+export const baron = {
+    "gold": 325,
+    "experience": 3800,
+}
+
+export const elder = {
+    "gold": 1100,
+    "experience": 650,
+}
+
+export const jungleItem = {
+    "gold": 20,
+    "experience": 15,
+}
